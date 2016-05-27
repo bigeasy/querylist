1cf6d6f06c5bde400adffb1def5e85c2c16cc542
Alan Gutierrez <alan@prettyrobots.com>
2016-05-27 13:04:52 -0500

    Rename to `stringify` and `parse`.

--- querylist.js
+++ querylist.js
@@ -1,12 +1,12 @@
-exports.encode = function (list) {
+exports.stringify = function (list) {
     return '?' + list.map(function (parameter) {
         return encodeURIComponent(parameter.name) + '=' + encodeURIComponent(parameter.value)
     }).join('&')
 }
 
-exports.decode = function (querystring) {
+exports.parse = function (querystring) {
     if (querystring[0] == '?') {
-        return exports.decode(querystring.substring(1))
+        return exports.parse(querystring.substring(1))
     }
     return querystring.split('&').map(function (part) {
         var pair = part.split('=')
